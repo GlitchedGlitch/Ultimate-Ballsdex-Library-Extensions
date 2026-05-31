03c82cd7bc9c6dbe07655ad60be0c5f5ebab217b
Glitch <126723902+GlitchedGlitch@users.noreply.github.com>
2026-05-31 18:02:05 +0200

    Create models.py

---
+++ packages/player/collector/models.py
@@ -0,0 +1,9 @@
+from ballsdex.core.models import Ball
+
+
+class CollectorBall(Ball):
+    class Meta:
+        proxy = True
+        verbose_name = "Collector Requirement"
+        verbose_name_plural = "Collector Requirements"
+        app_label = "collector_admin"
