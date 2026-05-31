cf2c6ec702361f13fa7730a2f4c7af98fbe1b9d5
Glitch <126723902+GlitchedGlitch@users.noreply.github.com>
2026-05-31 18:05:33 +0200

    Update installer.py

--- packages/player/rarity/installer.py
+++ packages/player/rarity/installer.py
@@ -2,7 +2,7 @@ import asyncio, base64, io, os, requests, traceback, discord
 from discord.ui import View, Button
 
 REPO = "GlitchedGlitch/Ultimate-Ballsdex-Library-Extensions"
-BASE = "https://api.github.com/repos/{}/contents/packages/player/rarity/{}".format(REPO, "{}")
+BASE = "https://api.github.com/repos/REPO/contents/packages/CATEGORY/PACKAGE/{}?ref=v2-main".format("{}")
 PKG = "/code/ballsdex/packages/rarity"
 CONFIG = "/code/config.yml"
 PACKAGE_ENTRY = "  - ballsdex.packages.rarity"
