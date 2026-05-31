5ef78066793625fb4fa30a508e01b0a2be461364
Glitch <126723902+GlitchedGlitch@users.noreply.github.com>
2026-05-31 18:07:40 +0200

    Update installer.py

--- packages/admin/leave/installer.py
+++ packages/admin/leave/installer.py
@@ -2,7 +2,7 @@ import asyncio, base64, io, os, requests, traceback, discord
 from discord.ui import View, Button
 
 REPO   = "GlitchedGlitch/Ultimate-Ballsdex-Library-Extensions"
-BASE = "https://api.github.com/repos/REPO/contents/packages/CATEGORY/PACKAGE/{}?ref=v2-main".format("{}")
+BASE = "https://api.github.com/repos/REPO/contents/packages/admin/leave/{}?ref=v2-main".format("{}")
 PKG    = "/code/ballsdex/packages/leave"
 CONFIG = "/code/config.yml"
 PACKAGE_ENTRY = "  - ballsdex.packages.leave"
