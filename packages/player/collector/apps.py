b1390d199517cd81a91abb75caa25b71263b2699
Glitch <126723902+GlitchedGlitch@users.noreply.github.com>
2026-06-07 17:36:48 +0200

    Update apps.py

--- packages/player/collector/apps.py
+++ packages/player/collector/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class CollectorAdminConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
     name = "collector_admin"
     verbose_name = "Collector"
