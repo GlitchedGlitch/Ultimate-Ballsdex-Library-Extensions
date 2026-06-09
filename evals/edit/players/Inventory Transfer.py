efea4603c54564e3d5eb053e8be8fdf2c18ab337
Glitch <126723902+GlitchedGlitch@users.noreply.github.com>
2026-06-09 15:46:39 +0200

    Refactor inventory transfer to use user IDs and async methods
    Updated player ID handling and improved async calls for inventory transfer.

--- evals/edit/players/Inventory Transfer.py
+++ evals/edit/players/Inventory Transfer.py
@@ -1,20 +1,18 @@
-# Note: This eval uses Player ID and not discord user ID!
-# To get the player id check evals/info/players/Player ID.py
-# Put the player id on line 8 and 9
+# Put the user id on line 6 and 7
 
 .eval
-from ballsdex.core.models import Player
+import asyncio
 
-oldPlayer = await Player.filter(discord_id=OldPlayerIDHere).first()
-newPlayer = await Player.filter(discord_id=NewPlayerIDHere).first()
+oldPlayer=await Player.objects.filter(discord_id=OldUserIDHere).afirst()
+newPlayer=await Player.objects.filter(discord_id=NewUserIDHere).afirst()
 
 if not oldPlayer or not newPlayer:
-    return "One of the players does not exist in the database."
+ return "One of the players does not exist in the database."
 
-balls = BallInstance.filter(player=oldPlayer)
+balls=await asyncio.to_thread(lambda:list(BallInstance.objects.filter(player=oldPlayer)))
 
-async for ball in balls:
-    ball.player = newPlayer
-    await ball.save()
+for ball in balls:
+ ball.player=newPlayer
+ await ball.asave()
 
 return "Inventory transferred successfully."
