85166e76c670f8fd74cd9486c3246fe3da74cc26
Glitch <126723902+GlitchedGlitch@users.noreply.github.com>
2026-06-09 15:05:38 +0200

    Implement catch speed adjustment in Catch Speed.py
    Add functionality to edit catch speed using ball ID.

---
+++ evals/edit/cards/Catch Speed.py
@@ -0,0 +1,9 @@
+# Edit the catch speed by putting the ball id in the right place
+# and the catch speed in milliseconds
+
+.eval
+from datetime import timedelta
+
+ball = await BallInstance.get(id=0xBallIdInCardHere)
+
+ball.spawned_time = ball.catch_date - timedelta(milliseconds=TimeInMilliseconds)
