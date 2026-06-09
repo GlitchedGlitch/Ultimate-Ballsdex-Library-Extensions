41da0d63fad4b230722f447b4dc2d9684966dfc8
Glitch <126723902+GlitchedGlitch@users.noreply.github.com>
2026-06-09 15:24:33 +0200

    Implement edit and remove special card features
    Added functionality to edit and remove special cards by name or ID.

--- evals/edit/cards/Edit Card Special.py
+++ evals/edit/cards/Edit Card Special.py
@@ -1,1 +1,25 @@
+# Edit Card Special by name
+.eval
+ball = await BallInstance.get(id=0xBallIDInCardHere)
 
+special = await Special.get(name="Special Name Here")
+ball.special = special
+
+await ball.save()
+
+# Edit card special by special ID
+.eval
+ball = await BallInstance.get(id=0xBallIDInCardHere)
+
+special = await Special.get(id=IDOfSpecialHere)
+ball.special = special
+
+await ball.save()
+
+# Remove Special from card
+.eval
+ball = await BallInstance.get(id=0xBallIDInCardHere)
+
+ball.special = None
+
+await ball.save()
