1fcbe8643cae5f1b118421c949c523998acaa8e3
Glitch <126723902+GlitchedGlitch@users.noreply.github.com>
2026-06-09 15:49:18 +0200

    Clarify user ID usage and update variable names
    Updated comments to clarify user ID usage and changed variable names to reflect user ID instead of player ID.

--- evals/edit/players/Inventory Transfer.py
+++ evals/edit/players/Inventory Transfer.py
@@ -1,12 +1,10 @@
-# Note: This eval uses Player ID and not discord user ID!
-# To get the player id check evals/info/players/Player ID.py
-# Put the player id on line 8 and 9
+# Put the user id on line 6 and 7
 
 .eval
 from ballsdex.core.models import Player
 
-oldPlayer = await Player.filter(discord_id=OldPlayerIDHere).first()
-newPlayer = await Player.filter(discord_id=NewPlayerIDHere).first()
+oldPlayer = await Player.filter(discord_id=OldUserIDHere).first()
+newPlayer = await Player.filter(discord_id=NewUserIDHere).first()
 
 if not oldPlayer or not newPlayer:
     return "One of the players does not exist in the database."
