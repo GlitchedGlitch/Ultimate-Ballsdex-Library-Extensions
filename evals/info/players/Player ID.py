7721dac09ce8e797ffd186af7c638a4775c5dd4e
Glitch <126723902+GlitchedGlitch@users.noreply.github.com>
2026-06-09 15:58:29 +0200

    Update Player ID.py

--- evals/info/players/Player ID.py
+++ evals/info/players/Player ID.py
@@ -1,5 +1,5 @@
-# Put the discord id of the player in line 2
+# Put the discord id of the player in line 4
 
 .eval
-player = await Player.filter(discord_id=DiscordIdHere).first()
+player = await Player.objects.filter(discord_id=EnterDiscordIDHere).afirst()
 return player.id if player else "Player not found"
