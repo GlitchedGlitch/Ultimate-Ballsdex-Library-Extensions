a79e1c9ceb3bcad5363f644f0d52a42580bb46d8
Glitch <126723902+GlitchedGlitch@users.noreply.github.com>
2026-06-09 14:54:34 +0200

    Refactor trade player assignment in Traded by Player.py
    Refactor trade player update to use async methods and added option to remove traded by.

--- evals/edit/cards/Traded by Player.py
+++ evals/edit/cards/Traded by Player.py
@@ -1,4 +1,7 @@
 # NOTE: This eval requires Player ID, not to be confused with discord user ID.
 # To get the Player ID check the view Player ID eval in evals/info/players/Player ID.py
 
-.eval await BallInstance.get(id=0xBallIDInCardHere).update(trade_player=await Player.get(id=PlayerIDHere))
+.eval ball=await BallInstance.objects.aget(id=0xBallIDInCardHere); ball.trade_player=await Player.objects.aget(id=PlayerIDHere); await ball.asave()
+
+# Alternative, remove traded by
+.eval ball=await BallInstance.objects.aget(id=0xBallIDInCardHere); ball.trade_player=None; await ball.asave()
