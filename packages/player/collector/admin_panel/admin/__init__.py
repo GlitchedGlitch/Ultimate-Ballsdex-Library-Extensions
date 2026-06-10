85958d8982341af444c5a45a1c4c344c511878c0
Glitch <126723902+GlitchedGlitch@users.noreply.github.com>
2026-06-10 22:21:52 +0200

    Update __init__.py

--- packages/player/collector/admin_panel/admin/__init__.py
+++ packages/player/collector/admin_panel/admin/__init__.py
@@ -1,35 +1,34 @@
 from django.contrib import admin
-from django.contrib.admin import AdminSite
 
 
-class ReorderedAdminSite(AdminSite):
-    """Custom admin site that reorders apps to place collector between bd_models and social_django"""
+_original_get_app_list = admin.site.get_app_list
 
-    def get_app_list(self, request, app_label=None):
-        app_list = super().get_app_list(request, app_label)
-        if app_label is not None:
-            return app_list
 
-        bd_models = None
-        collector_admin = None
-        others = []
+def _reordered_get_app_list(request, app_label=None):
+    """Monkey-patch that places collector_admin between bd_models and everything else."""
+    app_list = _original_get_app_list(request, app_label)
+    if app_label is not None:
+        return app_list
 
-        for app in app_list:
-            if app["app_label"] == "bd_models":
-                bd_models = app
-            elif app["app_label"] == "collector_admin":
-                collector_admin = app
-            else:
-                others.append(app)
+    bd_models = None
+    collector_admin = None
+    others = []
 
-        ordered = []
-        if bd_models:
-            ordered.append(bd_models)
-        if collector_admin:
-            ordered.append(collector_admin)
-        ordered.extend(others)
+    for app in app_list:
+        if app["app_label"] == "bd_models":
+            bd_models = app
+        elif app["app_label"] == "collector_admin":
+            collector_admin = app
+        else:
+            others.append(app)
 
-        return ordered
+    ordered = []
+    if bd_models:
+        ordered.append(bd_models)
+    if collector_admin:
+        ordered.append(collector_admin)
+    ordered.extend(others)
 
-admin.site = ReorderedAdminSite()
-admin.sites.site = admin.site
+    return ordered
+
+admin.site.get_app_list = _reordered_get_app_list
