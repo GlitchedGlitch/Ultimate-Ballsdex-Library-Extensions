a01fe4e21320f7cc4072b4530d10465462e496b6
Glitch <126723902+GlitchedGlitch@users.noreply.github.com>
2026-06-10 22:48:53 +0200

    Update __init__.py

--- packages/player/collector/admin_panel/admin/__init__.py
+++ packages/player/collector/admin_panel/admin/__init__.py
@@ -1,34 +1,55 @@
 from django.contrib import admin
 
 
-_original_get_app_list = admin.site.get_app_list
+_original_get_app_list = None
 
 
-def _reordered_get_app_list(request, app_label=None):
-    """Monkey-patch that places collector_admin between bd_models and everything else."""
-    app_list = _original_get_app_list(request, app_label)
-    if app_label is not None:
+def _patch_admin_app_list():
+    """Monkey-patch admin.site.get_app_list to place collector_admin after bd_models"""
+    global _original_get_app_list
+
+    if _original_get_app_list is not None:
+        return  
+
+    _original_get_app_list = admin.site.get_app_list
+
+    def _reordered_get_app_list(request, app_label=None):
+        app_list = _original_get_app_list(request, app_label)
+        if app_label is not None:
+            return app_list
+
+        bd_models_idx = None
+        collector_idx = None
+
+        for i, app in enumerate(app_list):
+            if app["app_label"] == "bd_models":
+                bd_models_idx = i
+            elif app["app_label"] == "collector_admin":
+                collector_idx = i
+
+        if collector_idx is not None and bd_models_idx is not None:
+
+            collector_app = app_list.pop(collector_idx)
+
+            if collector_idx < bd_models_idx:
+                bd_models_idx -= 1
+
+            app_list.insert(bd_models_idx + 1, collector_app)
+
         return app_list
 
-    bd_models = None
-    collector_admin = None
-    others = []
+    admin.site.get_app_list = _reordered_get_app_list
 
-    for app in app_list:
-        if app["app_label"] == "bd_models":
-            bd_models = app
-        elif app["app_label"] == "collector_admin":
-            collector_admin = app
-        else:
-            others.append(app)
+from django.apps import apps
 
-    ordered = []
-    if bd_models:
-        ordered.append(bd_models)
-    if collector_admin:
-        ordered.append(collector_admin)
-    ordered.extend(others)
+if apps.ready:
+    _patch_admin_app_list()
+else:
 
-    return ordered
+    from django.core.signals import setting_changed
+    from django.dispatch import receiver
 
-admin.site.get_app_list = _reordered_get_app_list
+    @receiver(setting_changed)
+    def _on_setting_changed(sender, **kwargs):
+        if apps.ready and _original_get_app_list is None:
+            _patch_admin_app_list()
