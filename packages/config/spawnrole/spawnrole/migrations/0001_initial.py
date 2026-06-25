6d1b25f4faf25cc1ccc7a340c90e0f1136f60a48
Glitch <126723902+GlitchedGlitch@users.noreply.github.com>
2026-06-25 12:11:33 +0200

    Update 0001_initial.py

--- packages/config/spawnrole/spawnrole/migrations/0001_initial.py
+++ packages/config/spawnrole/spawnrole/migrations/0001_initial.py
@@ -1,23 +1,22 @@
 from django.db import migrations, models
+import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
+
     initial = True
 
-    dependencies = []
+    dependencies = [
+        ("bd_models", "0001_initial"),
+    ]
 
     operations = [
         migrations.CreateModel(
             name="SpawnRole",
             fields=[
                 ("id", models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID")),
-                ("guild_id", models.BigIntegerField(
-                    unique=True,
-                    help_text="Discord guild ID this spawn role applies to.",
-                )),
-                ("role_id", models.BigIntegerField(
-                    help_text="Discord role ID to mention at the end of every spawn message.",
-                )),
+                ("role_id", models.BigIntegerField(help_text="Discord role ID to mention at the end of every spawn message.")),
+                ("guild", models.OneToOneField(help_text="The guild this spawn role belongs to.", on_delete=django.db.models.deletion.CASCADE, related_name="spawn_role", to="bd_models.guildconfig")),
             ],
             options={
                 "verbose_name": "Spawn Role",
