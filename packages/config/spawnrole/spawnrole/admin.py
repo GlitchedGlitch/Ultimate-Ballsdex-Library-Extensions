658f4c5c6b9f23a50afab631ca0f881b9b24615a
Glitch <126723902+GlitchedGlitch@users.noreply.github.com>
2026-06-25 12:30:52 +0200

    Update admin.py

--- packages/config/spawnrole/spawnrole/admin.py
+++ packages/config/spawnrole/spawnrole/admin.py
@@ -3,12 +3,14 @@ from bd_models.admin.guild import GuildAdmin
 from .models import SpawnRole
 
 
-class SpawnRoleInline(admin.StackedInline):
+class SpawnRoleInline(admin.TabularInline):
     model = SpawnRole
-    can_delete = False
+    can_delete = True
     verbose_name_plural = "Spawn Role"
     fields = ("role_id",)
     extra = 0
+    max_num = 1
+    min_num = 0
 
 if SpawnRoleInline not in getattr(GuildAdmin, "inlines", []):
     GuildAdmin.inlines = list(getattr(GuildAdmin, "inlines", [])) + [SpawnRoleInline]
