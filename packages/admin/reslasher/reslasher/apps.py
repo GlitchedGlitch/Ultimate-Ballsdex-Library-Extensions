fc3786c4a2b466438b363213ee972404dad87c4b
Glitch <126723902+GlitchedGlitch@users.noreply.github.com>
2026-07-06 16:11:55 +0200

    Create apps.py

---
+++ packages/admin/reslasher/reslasher/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class ReSlasherConfig(AppConfig):
+    name = "reslasher"
+    verbose_name = "ReSlasher"
+    default_auto_field = "django.db.models.BigAutoField"
+    dpy_package = "reslasher.reslasher"
