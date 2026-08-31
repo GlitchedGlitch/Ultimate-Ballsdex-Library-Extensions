03e4296afc5964bed4d922313b3419a138d88538
Glitch <126723902+GlitchedGlitch@users.noreply.github.com>
2026-08-31 21:32:41 +0200

    Delete packages/config/spawnrole directory

--- packages/config/spawnrole/spawnrole/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class SpawnRoleConfig(AppConfig):
-    name = "spawnrole"
-    verbose_name = "Spawn Role"
-    default_auto_field = "django.db.models.BigAutoField"
-    dpy_package = "spawnrole.spawnrole"
